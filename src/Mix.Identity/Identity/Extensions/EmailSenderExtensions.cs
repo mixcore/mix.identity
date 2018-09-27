c56e66436b2edefbaf32e017ff94c112ad3e8878
Huy Nguyen <i.love.to.smile.around@gmail.com>
2018-09-28 00:25:36 +0700

    upd
    Co-Authored-By: Nhật Hoàng <nhathoang989@gmail.com>

--- src/Mix.Identity/Identity/Extensions/EmailSenderExtensions.cs
+++ src/Mix.Identity/Identity/Extensions/EmailSenderExtensions.cs
@@ -1,7 +1,8 @@
-// Licensed to the Mixcore Foundation under one or more agreements.
-// The Mixcore Foundation licenses this file to you under the GNU General Public License v3.0.
+// Licensed to the Swastika I/O Foundation under one or more agreements.
+// The Swastika I/O Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.AspNetCore.Identity.UI.Services;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
